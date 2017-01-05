2fa43dc81c9c444bccbbb69c940e82390fa86824
zhenshenghe <hezs1994@outlook.com>
2017-01-06 03:46:24 +0800

    1到N之间整数含1的个数

---
+++ src/_32_NumberOfBetween1AndN.java
@@ -0,0 +1,23 @@
+/**
+ * Created by hezs1 on 2017/1/6.
+ */
+/*
+求出1~13的整数中1出现的次数,并算出100~1300的整数中1出现的次数？为此他特别数了一下
+1~13中包含1的数字有1、10、11、12、13因此共出现6次,但是对于后面问题他就没辙了。
+ACMer希望你们帮帮他,并把问题更加普遍化,可以很快的求出任意非负整数区间中1出现的次数。
+ */
+public class _32_NumberOfBetween1AndN {
+    public int NumberOf1Between1AndN_Solution(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+           int curNumber=i;
+            while (curNumber!=0){
+                if(curNumber%10==1)
+                    count++;
+                curNumber/=10;
+            }
+        }
+        return count;
+    }
+}
+//还有种方法以后再补上
